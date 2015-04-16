062111aedb28e731b29f8b04eea5d312c55df712
akshaybhandari <akshayb.2693@gmail.com>
2015-04-16 20:26:44 +0530

    Modified profile classes to install necessary packages and make all configuration changes

--- manifests/profile/nagioscontroller.pp
+++ manifests/profile/nagioscontroller.pp
@@ -1,12 +1,39 @@
 class nagiosopenstack::profile::nagioscontroller {
+  # install package nagios-nrpe-server
   package { 'nagios-nrpe-server':
     ensure => latest,
   }
+
+  # add nagios users to sudoers
   file { '/etc/sudoers.d/nagios_sudoers':
     ensure => present,
-    source => 'puppet:///modules/files/nagios_sudoers',
+    source => 'puppet:///modules/nagiosopenstack/nagios_sudoers',
     owner  => 'root',
     group  => 'root',
     mode   => '0440',
+  }
+
+  # nagios-nrpe configuration to allow nagios-server
+  file { '/etc/nagios/nrpe.cfg':
+    ensure  => present,
+    source  => 'puppet:///modules/nagiosopenstack/nrpe.cfg',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => Package['nagios-nrpe-server'],
+  } ->
+  file_line { '/etc/nagios/nrpe.cfg':
+    path   => '/etc/nagios/nrpe.cfg',
+    line   => "allowed_hosts=127.0.0.1,${::nagiosopenstack::config::nagios_server}",
+    match  => 'allowed_hosts=*',
+    notify => Service['nagios-nrpe-server'],
+  }
+
+  # Restart nagios-nrpe-server
+  service { 'nagios-nrpe-server':
+    ensure     => running,
+    hasstatus  => true,
+    hasrestart => true,
+    require    => Package['nagios-nrpe-server'],
   }
 }
